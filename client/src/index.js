e840f3698362b62120c284c83a1101504ffb0952
João Andreassa <100442846+JoaoAndreassa@users.noreply.github.com>
2023-01-28 20:58:36 -0300

    a

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,7 @@ import './formulario/form.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />.
-    <Form />
+    <App />
   </React.StrictMode>
 );
 
